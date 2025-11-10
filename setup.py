911d83a9cbd71733132d2a6afdb1b4db33fe08cc
OlesProkopiv <143633603+OlesProkopiv@users.noreply.github.com>
2025-11-10 17:14:37 +0200

    Update version and add Test PyPI publish

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="my-tail",
-    version="0.1.0",
+    version="0.1.1",
     description="A small implementation of Unix tail (supports -n and -f) implemented with Click",
     long_description=README,
     long_description_content_type="text/markdown",
-    author="Ваше Ім'я",
+    author="Олесь Прокопів",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
